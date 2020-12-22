1b0de4d9515767d19ec3fa197f162f3acd2a4a62
Bill Lytton; cell 917 613 3991 <neuro014@gmail.com>
2020-12-22 16:18:31 -0500

    get rid of extra lines

--- ragp.py
+++ ragp.py
@@ -4,7 +4,5 @@ soma = h.Section(name='soma')
 soma.L, soma.diam, soma.cm = 12.6157, 12.6157, 1
 
 soma.insert('hh')
-hh.gnabar = 0.12 # sodium condundtance in S/cm^2
-hh.gkbar = 0.036 # potassium conductance in S/cm^2
-hh.gl = 0.0003   # leak conductance in S/cm2
-hh.el = -54.3 # reversal potential in mV
+hh.gnabar, hh.gkbar, hh.gl, hh.el = 0.12 , 0.036 , 0.0003 , -54.3
+
