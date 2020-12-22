0a2f1166d5f09b0fa09f9ef4fa3d27a5679f237d
Bill Lytton; cell 917 613 3991 <neuro014@gmail.com>
2020-12-22 15:41:48 -0500

    get rid of comments and print statements

--- ragp.py
+++ ragp.py
@@ -1,52 +1,12 @@
 from neuron import h,gui
-
-
 soma = h.Section(name='soma')
-### h.topology()
+# soma.psection()['morphology']['L']
 
-### somttpsection()
-#{'point_processes': {}, 
-#'density_mechs': {}, 
-#'ions': {}, 
-#'morphology': {'L': 100.0, 
-#'diam': [500.0], 
-#'pts3d': [], 
-#'parent': None, 
-#'trueparent': None}, 
-#'nseg': 1, 
-#'Ra': 35.4, 
-#'cm': [1.0], 
-#'regions': set(), 
-#'species': set(), 
-#'name': 
-#'soma', 
-#'hoc_internal_name': 
-#'__nrnsec_0x7ff4af765000', 
-#'cell': None}
-
-# pprint.pprint
-soma.psection()['morphology']['L']
-
-# VALS TO CHANGE IN NEURON t
-#h.celsius
-#soma.Ra
-
-
-
-### SETTING VALUES
+# SETTING VALUES
 soma.L = []
 soma.diam = []
-# dir(soma)
-#import textwrap
-#print(textwrap.fill(', '.join(dir(h))))
-# dir()
-# help()
 
-### INSERT ION CHANNELS
+# INSERT ION CHANNELS
 soma.insert('hh')
 
-
-### SECTIONS AND SEGMENTS ###
-# Section: section
-# Segment: segment
-print("type(soma)={}".format(type(soma)))
+# SECTIONS AND SEGMENTS ###
